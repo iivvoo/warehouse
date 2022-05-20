6a92dab3d061be9204cd3aef572592d1f3e33cf6
Ivo van der Wijk <github@in.m3r.nl>
2022-05-20 16:00:51 +0200

    update main

--- main.go
+++ main.go
@@ -7,10 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
-
 	cache := warehouse.New[string, string]()
 	cache.Set("foo", "hello")
-	fmt.Println(cache, cache.Get("foo"))
-	cache.Cleanup()
+	fmt.Println(cache.Get("foo"))
 }
